ee35285384644fa53196292cf24e278b6786631b
ARKTD <support@dssoftware.ru>
2025-10-10 19:29:24 +0500

    change the JSDoc to actually reflect what it returns

--- src/discord/modules/SafeDiscord.js
+++ src/discord/modules/SafeDiscord.js
@@ -4,14 +4,15 @@ module.exports = {
     /**
      * @param {TextChannel} channel 
      * @param {string | MessagePayload | MessageCreateOptions} options 
-     * @return {Promise<Message<true>>}
+     * @return {Promise<Message<true>|undefined>}
      */
     async send(channel, options){
         try{
-            await channel.send(options);
+            return await channel.send(options);
         }
         catch(e){
             // Ignore error, safe discord was used to ignore this error.
+            return undefined;
         }
     }
 };
